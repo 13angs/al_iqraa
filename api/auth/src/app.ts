601fb09829ed3021cf616d008515aff7f053b9f2
13angs <13angz.um@gmail.com>
2022-05-14 15:23:14 +0000

    test the auth api

---
+++ api/auth/src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: false
+    })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+
+// if using async
+// need to throw the error in next(new Error());
+// else
+// use the express-async-errors package
+// to handle the errors
+app.all("*", async () => {
+    throw new NotFoundError();
+})
+
+
+// register the error handlers here
+app.use(errorHandler);
+
+export { app };
--- api/auth/src/index.ts
+++ api/auth/src/index.ts
@@ -2,48 +2,8 @@
 // init the tsconfig
 // tsc --init
 
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: false
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-
-// if using async
-// need to throw the error in next(new Error());
-// else
-// use the express-async-errors package
-// to handle the errors
-app.all("*", async () => {
-    throw new NotFoundError();
-})
-
-
-// register the error handlers here
-app.use(errorHandler);
-
+import { app } from './app';
 // connecto to mongo db
 const start = async () => {
     if (!process.env.JWT_KEY) {
