9f9679b6d6b151900dcd935f2ba362ea010e0927
13angs <13angz.um@gmail.com>
2022-05-18 01:23:46 +0700

    create register page and connect some api

--- next-app/apps/auth/pages/register/index.tsx
+++ next-app/apps/auth/pages/register/index.tsx
@@ -1,43 +1,46 @@
 // import styles from './index.module.css';
 import * as React from 'react';
-import { Fab } from '@next-app/fab';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 
-export function Index() {
-  const router = useRouter();
-  const [login, setLogin] = React.useState({
+/* eslint-disable-next-line */
+export interface RegisterProps { }
+
+export function Register(props: RegisterProps) {
+  const [register, setRegister] = React.useState({
     email: '',
-    password: 'password'
-  })
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
-  const handleInput = (e) => {
-    setLogin(prev => ({
+    password: ''
+  });
+
+
+  const handleInputdata = (e) => {
+    setRegister(prev => ({
       ...prev,
       [e.target.name]: e.target.value
     }))
   }
-  const handleLoginSubmit = (e) => {
-    e.preventDefault();
-    axios.post('http://al-iqraa.com/api/v1/auth/signin', {
-      email: login.email,
-      password: login.password
-    }).then(res => {
-      console.log(res);
-    })
-  }
+
+  const router = useRouter();
+  const { query } = router;
+
+  const userEmail = React.useMemo(() => {
+    if (query) {
+      return query.email;
+    }
+    return null;
+  }, [query]);
+
   return (
-    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm m-auto mt-40">
+    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm m-auto mt-52">
       <h1 className="text-center text-light-text text-xl font-bold mb-5">Welcome to Al Iqraa</h1>
-      <p className="text-gray-500 my-2">Please sign-in to your account and start the adventure</p>
-      <form onSubmit={handleLoginSubmit}>
+      <form>
         <div className="form-group mb-6">
-          <label htmlFor="exampleInputEmail2" className="form-label inline-block mb-2 text-light-text">Email address</label>
-          <input type="email" className="form-control
+          {/* <label htmlFor="exampleInputEmail2" className="form-label inline-block mb-2 text-light-text">Email address</label> */}
+          <input
+            type={userEmail ? 'password' : "email"}
+            name={userEmail ? 'password' : "email"}
+            required
+            className="form-control
             block
             w-full
             px-3
@@ -52,13 +55,13 @@ export function Index() {
             ease-in-out
             m-0
             focus:text-light-text focus:bg-white focus:border-primary focus:outline-none" id="exampleInputEmail2"
-            aria-describedby="emailHelp" placeholder="Enter email"
-            name='email'
-            onChange={handleInput}
-            value={login.email}
+            aria-describedby="emailHelp"
+            placeholder={`Enter ${userEmail ? 'password' : 'email'}`}
+            value={userEmail ? register.password : register.email}
+            onChange={handleInputdata}
           />
         </div>
-        <div className="form-group mb-6">
+        {/* <div className="form-group mb-6">
           <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-light-text">Password</label>
           <input type="password" className="form-control block
             w-full
@@ -74,11 +77,7 @@ export function Index() {
             ease-in-out
             m-0
             focus:text-light-text focus:bg-white focus:border-primary focus:outline-none" id="exampleInputPassword2"
-            placeholder="Password"
-            name="password"
-            onChange={handleInput}
-            value={login.password}
-          />
+            placeholder="Password" />
         </div>
         <div className="flex justify-between items-center mb-6">
           <div className="form-group form-check">
@@ -90,8 +89,8 @@ export function Index() {
           <a href="#!"
             className="text-primary hover:text-primary focus:text-primary transition duration-200 ease-in-out">Forgot
             password?</a>
-        </div>
-        <button type="submit" className="
+        </div> */}
+        <button type={userEmail ? 'submit' : 'button'} className="
           w-full
           px-6
           py-2.5
@@ -108,16 +107,37 @@ export function Index() {
           active:bg-blue-800 active:shadow-lg
           transition
           duration-150
-          ease-in-out">Sign in</button>
+          ease-in-out"
+          onClick={(e) => {
+            e.preventDefault();
+
+            if (userEmail) {
+              e.preventDefault();
+              console.log(query.email, register.password);
+              axios.post('http://al-iqraa.com/api/v1/auth/signup', {
+                email: query.email,
+                password: register.password
+              }).then(res => {
+                console.log(res);
+              })
+            }
+            // send the user/email to the route
+            // switch to password
+            router.push({
+              pathname: '/register',
+              query: { email: register.email }
+            })
+          }}
+        >Continue with Email</button>
         <p className="mt-6 text-center cursor-pointer"><a
-          onClick={() => router.push('/register')}
-          className="text-primary hover:text-primary focus:text-primary transition duration-200 ease-in-out">Continue with Email</a>
+          onClick={() => router.push('/')}
+          className="text-primary hover:text-primary focus:text-primary transition duration-200 ease-in-out">Other Login Options</a>
         </p>
       </form>
 
-      <Fab />
+      {/* <Fab /> */}
     </div>
   );
 }
 
-export default Index;
+export default Register;
